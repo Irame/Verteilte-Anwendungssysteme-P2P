8f2aea95938570f5dc34d83bc62e6e5971eb7288
MBulli <markusbullmann@gmail.com>
2016-12-07 19:39:48 +0100

    Added ChordDisplay project

---
+++ ChordDisplay/ChordDisplay/ChordNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChordDisplay
+{
+    public class ChordNode : PropertyChangedBase
+    {
+        string id;
+        public string ID
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        string ip;
+        public string IP
+        {
+            get { return ip; }
+            set { ip = value; OnPropertyChanged(); }
+        }
+    }
+}
---
+++ ChordDisplay/ChordDisplay/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ChordDisplay
+{
+    public class MainViewModel : PropertyChangedBase
+    {
+        FileSystemWatcher fileWatcher;
+
+        public ObservableCollection<ChordNode> Nodes
+        {
+            get;
+        } = new ObservableCollection<ChordNode>();
+
+
+        public MainViewModel()
+        {
+            fileWatcher = new FileSystemWatcher(@"C:\Temp\", "*.csv");
+            fileWatcher.Changed += FileWatcher_Changed;
+            fileWatcher.EnableRaisingEvents = true;
+
+            UpdateRing();
+        }
+
+        private void FileWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name == "AllNodes.csv" && e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    UpdateRing();
+                });
+            }
+        }
+
+        private void UpdateRing()
+        {
+            Nodes.Clear();
+
+            foreach (var node in ReadFile(@"C:\Temp\AllNodes.csv"))
+            {
+                Nodes.Add(node);
+            }
+        }
+
+        private IEnumerable<ChordNode> ReadFile(string fullFilename)
+        {
+            foreach (var line in File.ReadAllLines(fullFilename))
+            {
+                var split = line.Split(';');
+
+                var hash = split[0];
+                var url = split[1];
+
+                yield return new ChordNode() { ID = hash, IP = url };
+            }
+        }
+    }
+}
