78dc6288f643d6c7246e5f69fb997b77f43fdfe5
Irame <irame94@gmail.com>
2016-12-12 19:02:01 +0100

    Added Nancy Server Project for key sharing

---
+++ NancyServerChord/NancyServerChord/EntryInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NancyServerChord
+{
+    enum DataType
+    {
+        String, File
+    }
+
+    class EntryInfo
+    {
+        public string Person;
+        public string KeyName;
+        public DataType DataType;
+    }
+}
